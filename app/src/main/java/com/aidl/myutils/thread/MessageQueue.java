489691b28ec9ce48a18e1864c10dd280884a68bc
evan <evan@example.com>
2023-04-21 11:22:27 +0800

    添加三个线程的队列例子

--- app/src/main/java/com/aidl/myutils/thread/MessageQueue.java
+++ app/src/main/java/com/aidl/myutils/thread/MessageQueue.java
@@ -1,2 +1,79 @@
-package com.aidl.myutils.thread;public class MessageQueue {
+package com.aidl.myutils.thread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 先进先出消息队列，三个线程同时发送消息，只保存时间最近发送的消息，其他丢弃
+ * 输出：Received message from thread: Thread 1, message: Hello, World!，每次不一样
+ */
+public class MessageQueue {
+
+    private BlockingQueue<Message> queue;
+
+    private static class Message {
+        private String message;
+        private String senderThreadName;
+
+        public Message(String message, String senderThreadName) {
+            this.message = message;
+            this.senderThreadName = senderThreadName;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getSenderThreadName() {
+            return senderThreadName;
+        }
+    }
+
+    public MessageQueue() {
+        this.queue = new LinkedBlockingQueue<>(1); // 限制队列大小为1，以实现只保存最近的一个消息
+    }
+
+    public void sendMessage(String message) {
+        try {
+            String senderThreadName = Thread.currentThread().getName();
+            Message newMessage = new Message(message, senderThreadName);
+            this.queue.put(newMessage); // 将消息添加到队列中
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveMessage() {
+        try {
+            Message message = this.queue.take(); // 获取队列中的消息，如果队列为空则会阻塞
+            System.out.println("Received message from thread: " + message.getSenderThreadName() + ", message: " + message.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MessageQueue messageQueue = new MessageQueue();
+
+        Thread thread1 = new Thread(() -> {
+            messageQueue.sendMessage("Hello, World!");
+        }, "Thread 1");
+
+        Thread thread2 = new Thread(() -> {
+            messageQueue.sendMessage("Hello, World!");
+        }, "Thread 2");
+
+        Thread thread3 = new Thread(() -> {
+            messageQueue.sendMessage("Hello, World!");
+        }, "Thread 3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        messageQueue.receiveMessage();
+    }
 }
+
+
+
--- app/src/main/java/com/aidl/myutils/thread/MessageQueueFIFO.java
+++ app/src/main/java/com/aidl/myutils/thread/MessageQueueFIFO.java
@@ -27,10 +27,10 @@ import java.util.Queue;
  * 2023-04-21 10:49:55.777 28236-28273 System.out              com.aidl.myutils                     I  Sent: Message 10
  * 2023-04-21 10:49:55.777 28236-28274 System.out              com.aidl.myutils                     I  Received: Message 10
  */
-public class MessageQueue {
+public class MessageQueueFIFO {
     private Queue<String> queue;
 
-    public MessageQueue() {
+    public MessageQueueFIFO() {
         queue = new LinkedList<>();
     }
 
@@ -47,7 +47,7 @@ public class MessageQueue {
     }
 
     public static void main(String[] args) {
-        MessageQueue messageQueue = new MessageQueue();
+        MessageQueueFIFO messageQueue = new MessageQueueFIFO();
 
         // Sender thread sends messages to the queue
         Thread senderThread = new Thread(() -> {
