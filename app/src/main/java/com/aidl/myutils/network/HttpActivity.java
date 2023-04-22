d69401a2ced0c278aee0d21702361242a3b68ab2
michael <yhm2046@gmail.com>
2023-04-23 01:56:45 +0800

    添加http，okhttp的协议使用方法

--- app/src/main/java/com/aidl/myutils/network/HttpActivity.java
+++ app/src/main/java/com/aidl/myutils/network/HttpActivity.java
@@ -3,6 +3,7 @@ package com.aidl.myutils.network;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -17,24 +18,36 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * http example,no using android:usesCleartextTraffic="true" in AndroidManifest
+ */
 public class HttpActivity extends AppCompatActivity implements View.OnClickListener {
     ActivityHttpBinding activity_net;
-    private static final String YOUTUBE_WEB = "http://www.youtube.com";
+    private static final String TAG = "evan";
+    private static final String YOUTUBE_WEB = "https://www.youtube.com";
+    private static final String BING_WEB = "https://www.bing.com";
+    private static final String BAIDU_WEB = "https://www.baidu.com";
+    private static final int NETWORK_TIME_OUT = 3 * 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity_net  = ActivityHttpBinding.inflate(LayoutInflater.from(this));
         setContentView(activity_net.getRoot());
+        activity_net.textView.setText(HttpActivity.class.getName());
         activity_net.button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
        if (view.getId() == R.id.button){
+           Log.i(TAG,"click..");
            sendRequestWithHttpURLConnection();
        }
     }
 
+    /**
+     *  java.util.concurrent.TimeoutException 报错
+     */
     private void sendRequestWithHttpURLConnection(){
         new Thread(new Runnable() {
             @Override
@@ -42,36 +55,48 @@ public class HttpActivity extends AppCompatActivity implements View.OnClickListe
                 HttpURLConnection connection = null;
                 BufferedReader reader = null;
                 try {
-                    URL url = new URL(YOUTUBE_WEB);
+                    URL url = new URL(BAIDU_WEB);
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8 * 1000);
-                    connection.setReadTimeout(8 * 1000);
-                    InputStream in = connection.getErrorStream();
-//                    读取输入流
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuffer response = new StringBuffer();
+                    connection.setConnectTimeout(NETWORK_TIME_OUT);
+                    connection.setReadTimeout(NETWORK_TIME_OUT);
+                    int responseCode = connection.getResponseCode(); //发送http请求
+                    InputStream in = null;
+                    switch (responseCode){
+                        case HttpURLConnection.HTTP_NOT_FOUND:
+                            Log.i(TAG,"404 error");
+                            break;
+                        case HttpURLConnection.HTTP_INTERNAL_ERROR:
+                            Log.i(TAG,"500 error");
+                            break;
+                        default:
+                            in = connection.getInputStream();
+                    }
+                    reader = new BufferedReader(new InputStreamReader(in)); //流读取
+                    StringBuilder response = new StringBuilder();
                     String line;
                     while ((line = reader.readLine()) != null){
                         response.append(line);
                     }
-                    assert line != null;
-                    showResponse(line.toString());
+                    showResponse(response.toString());
                 } catch (Exception e) {
+                    Log.i(TAG,"Exception.." + e);
                     throw new RuntimeException(e);
                 }finally {
                     if (reader != null){
                         try {
                             reader.close();
                         }catch (IOException e){
+                            Log.i(TAG,"Exception.." + e);
                             e.printStackTrace();
                         }
                     }if (connection != null){
+                        Log.i(TAG,"connection null" );
                         connection.disconnect();
                     }
                 }
             }
-        });
+        }).start();
     }   //sendRequestWithHttpURLConnection
 
     private void showResponse(final String response){
@@ -79,6 +104,7 @@ public class HttpActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void run() {
                 activity_net.textView.setText(response);
+                Log.i(TAG,"response:" + response);
             }
         });
     }
---
+++ app/src/main/java/com/aidl/myutils/network/OkHttpActivity.java
@@ -0,0 +1,88 @@
+package com.aidl.myutils.network;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aidl.myutils.R;
+import com.aidl.myutils.databinding.ActivityHttpBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * okhttp的例子，首先需要确保设备连接网络正常，否则会报错：
+ * java.net.UnknownHostException: Unable to resolve host "www.baidu.com": No address associated with hostname
+ * reference：<a href="https://blog.csdn.net/weixin_33788244/article/details/94032744">...</a>
+ *
+ * if not using android:usesCleartextTraffic="true" in AndroidManifest
+ * will have error:
+ * java.lang.RuntimeException: java.net.UnknownServiceException: CLEARTEXT communication to www.baidu.com not permitted by network security policy
+ */
+public class OkHttpActivity extends AppCompatActivity implements View.OnClickListener {
+    ActivityHttpBinding activity_net;
+    private static final String TAG = "evan";
+    private static final String YOUTUBE_WEB = "https://www.youtube.com";
+    private static final String BING_WEB = "https://www.bing.com";
+    private static final String BAIDU_WEB = "http://www.baidu.com";
+    private static final int NETWORK_TIME_OUT = 3 * 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity_net  = ActivityHttpBinding.inflate(LayoutInflater.from(this));
+        setContentView(activity_net.getRoot());
+        activity_net.textView.setText(OkHttpActivity.class.getName());
+        activity_net.button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+       if (view.getId() == R.id.button){
+           Log.i(TAG,"click..");
+           sendRequestWithOkHttp();
+       }
+    }
+
+    /**
+     *  java.util.concurrent.TimeoutException 报错
+     */
+    private void sendRequestWithOkHttp(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url(BAIDU_WEB).build(); //request对象
+                    Response response = client.newCall(request).execute();  //发送请求并返回数据
+                    assert response.body() != null;
+                    String responseDate = response.body().string();
+                    showResponse(responseDate);
+                } catch (Exception e) {
+                    Log.i(TAG,"Exception:" + e);
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }   //sendRequestWithHttpURLConnection
+
+    private void showResponse(final String response){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                activity_net.textView.setText(response);
+                Log.i(TAG,"response:" + response);
+            }
+        });
+    }
+}
