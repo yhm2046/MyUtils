fd0aa522f54901a86bbc9553dbcaf2d4ab05626a
michael <yhm2046@gmail.com>
2023-05-31 12:17:02 +0800

    add linkList test

--- app/src/main/java/com/aidl/myutils/MainActivity.java
+++ app/src/main/java/com/aidl/myutils/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.VolumeShaper;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.aidl.myutils.colections.LinkedListTest;
 import com.aidl.myutils.date.DateCalculator;
 import com.aidl.myutils.designpatterns.simple.factory.Operation;
 import com.aidl.myutils.designpatterns.simple.factory.OperationFactory;
@@ -28,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
 //            Operation op = com.aidl.myutils.designpatterns.factory.method.OperationFactory.createOperation("pow");
 //            double result = op.getResult(2.0, 10);
 //            Log.i(TAG,"result :" + result);
-            FutureExample.main(null);
+//            FutureExample.main(null);
+            LinkedListTest.main(null);
         } catch (Exception e) {
             Log.i(TAG,"exception:" + e);
             throw new RuntimeException(e);
